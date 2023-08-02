fcf886c5c0016abec15fb33449239814c3e42911
Sérgio Luís de Azevedo Cruz Filho <54141596+sergioluisfilho@users.noreply.github.com>
2023-08-01 23:44:06 -0300

    Add session expired redirecting interceptor

--- src/api/axios.js
+++ src/api/axios.js
@@ -10,6 +10,27 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const originalRequest = error.config;
+    if (
+      !(
+        error.response &&
+        (error.response.status === 401 || error.response.status === 403) &&
+        originalRequest.url !== "/auth/login"
+      )
+    ) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      alert("Session expired, redirecting to login");
+      window.location.href = "/login";
+    }
+  }
+);
+
 export const setAuthorizationToken = (token) => {
   api.interceptors.request.use((config) => {
     config.headers["Authorization"] = token;
