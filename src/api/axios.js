201d4c2ab6ba15070b4402c730905622565634fc
Sérgio Luís de Azevedo Cruz Filho <54141596+sergioluisfilho@users.noreply.github.com>
2023-08-01 23:25:45 -0300

    Fix request broken after reload page

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,6 +5,11 @@ const api = axios.create({
   baseURL: "http://localhost:3000", // Substitua pela URL da sua API
 });
 
+api.interceptors.request.use((config) => {
+  config.headers["Authorization"] = localStorage.getItem("token");
+  return config;
+});
+
 export const setAuthorizationToken = (token) => {
   api.interceptors.request.use((config) => {
     config.headers["Authorization"] = token;
