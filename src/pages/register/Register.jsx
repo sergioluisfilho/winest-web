f5444199623ce3d8ca7af98dceccfd402bcaf446
Sérgio Luís de Azevedo Cruz Filho <54141596+sergioluisfilho@users.noreply.github.com>
2023-08-17 14:08:06 -0300

    try

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import api from "../../api/axios";
 import "./register.scss";
@@ -37,35 +37,23 @@ const Register = () => {
   }
 
   return (
-    <div className="register">   
-        <h1 className="title">Winest</h1>   
+    <div className="register-page">   
+      <h1 className="title">Winest</h1>   
       <section className="container">
         <form className="credential-form">
-          <input className="credential-input" type="text" placeholder="Full name" name="fullname" onChange={handleChange} />
-          </form>
-        <form className="credential-form">
           <input className="credential-input" type="text" placeholder="Name" name="name" onChange={handleChange} />
-          </form>
-          <form className="credential-form">
           <input className="credential-input" type="text" placeholder="Email" name="email" onChange={handleChange} />
-          </form>
-        <form className="credential-form">
           <input className="credential-input" type="password" placeholder="Password" name="password" onChange={handleChange} />
-          </form>
-          <form className="credential-form">
           <input className="credential-input" type="text" placeholder="Birth Date" name="birthdayDate" onChange={handleChange} />
-          </form>
-        <form className="credential-form">
           <input className="credential-input" type="text" placeholder="Phone number" name="phoneNumber" onChange={handleChange} />
-          </form>
           <button className="register-btn" onClick={handleClick}>Sign up</button>
+        </form>
         <div className="signin-content">
-          <span className="signin-desc">Already have an account?</span>
-          <span className="signin-desc"> </span>
+          <span className="signin-desc">Already have an account? </span>
           <Link className="link" to="/login">
           <span className="signin-btn">Sign in</span>
           </Link>
-        </div>
+          </div>
       </section>
     </div >
   );
